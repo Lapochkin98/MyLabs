b58d91ede9f6e78eddf02b36a0d7b5371ed2f847
Lapochkin98 <90198600+Lapochkin98@users.noreply.github.com>
2021-09-23 16:44:40 +0300

    Add files via upload

---
+++ Untitled-8.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+void main()
+{
+	STARTUPINFO cif;
+	ZeroMemory(&cif,sizeof(STARTUPINFO));
+	PROCESS_INFORMATION pi;
+	if (CreateProcess("c:\\windows\\notepad.exe",NULL,
+		NULL,NULL,FALSE,NULL,NULL,NULL,&cif,&pi)==TRUE)
+	{
+		cout << "process" << endl;
+		cout << "handle " << pi.hProcess << endl;
+		Sleep(1000);				
+		TerminateProcess(pi.hProcess,NO_ERROR);	// 
+	}
+}
