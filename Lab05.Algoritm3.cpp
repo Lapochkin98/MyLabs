896ccaffae5320529717cc0175be33013f79fbf3
Lapochkin98 <90198600+Lapochkin98@users.noreply.github.com>
2021-09-23 14:54:42 +0300

    Update Lab05.Algoritm3.cpp

--- Lab05.Algoritm3.cpp
+++ Lab05.Algoritm3.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main(){
-  char user1, user2;
+  char user1, user2, x;
   string name1, name2;
+  cout << "\nНачать игру?\n Y- да N- нет ";
+  cin >> x;
+  if (x == 'n' ){
+    exit(1);
+  }
+  else {
   cout << "Первый игрок введите своё имя: ";
   cin >> name1;
   cout << "Второй игрок введите своё имя: ";
   cin >> name2;
   while (true) {
-  cout << "R-камень" << endl << "P-бумага" << endl << "S-ножницы" << endl << "Первый игрок делает ход: ";
+  cout << "r-камень" << endl << "p-бумага" << endl << "s-ножницы" << endl << "Первый игрок делает ход: ";
   cin >> user1;
   cout << "Второй игрок делает ход: ";
   cin >> user2;
@@ -21,6 +28,11 @@ int main(){
       cout << "Выигрывает второй игрок, под именем: " << name2 << endl;
     } else if (user2 ==  's') {
       cout << "Выигрывает первый игрок под именем: " << name1 << endl; }
+      cout << "\nНачать игру?\n Y- да N- нет ";
+  cin >> x;
+  if (x == 'n' ){
+    exit(1);
+  }
     break;
     case 'p':
     if (user2 == 'p') {
@@ -29,6 +41,11 @@ int main(){
       cout << "Выигрывает второй игрок, под именем: " << name2 << endl;
     } else if (user2 ==  'r') {
       cout << "Выигрывает первый игрок под именем: "<< name1 << endl; }
+      cout << "\nНачать игру?\n Y- да N- нет ";
+  cin >> x;
+  if (x == 'n' ){
+    exit(1);
+  }
     break;
     case 's':
     if (user2 == 's') {
@@ -37,5 +54,10 @@ int main(){
       cout << "Выигрывает первый игрок под именем: " << name1 << endl ;
     } else if (user2 ==  'r') {
       cout << "Выигрывает второй игрок под именем: " << name2 << endl; }
-    break;
+      cout << "\nНачать игру?\n Y- да N- нет ";
+  cin >> x;
+  if (x == 'n' ){
+    exit(1);
+  }
+    break;}
 } } return 0; }
