9139055268b38561b36f1dda77856b27ffe89566
Lapochkin98 <90198600+Lapochkin98@users.noreply.github.com>
2022-09-19 10:51:06 +0300

    19.09.2022

---
+++ C#works/Lab4/Lab.work.2/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double user_numbers, admin_arifmetic = 0, number_count = -1, min_number = double.MaxValue, minus_count = 0, plus_count = 0;
+        Console.WriteLine("Введите числа: ");
+        do
+        {
+            user_numbers = double.Parse(Console.ReadLine());
+            admin_arifmetic += user_numbers;
+            number_count++;
+            if (user_numbers < min_number && user_numbers != 0)
+            {
+                min_number = user_numbers;
+            }
+            if (user_numbers < 0 & user_numbers !=0)
+            {
+                minus_count++;
+            }
+            else if (user_numbers > 0 & user_numbers !=0)
+            {
+                plus_count++;
+            }
+        }
+        while (user_numbers != 0);
+        Console.WriteLine("Количество цифр: " + number_count);
+        Console.WriteLine("Среднее арифметическое: " + admin_arifmetic/number_count);
+        Console.WriteLine("Наименьшее число: " + min_number);
+        Console.WriteLine("Количество отрицательных значений: " + minus_count);
+        Console.WriteLine("Количество положительных значений: " + plus_count);
+
+    }
+}
---
+++ C#works/Lab4/Lab.work.3/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double n, sum = 0, eps = 0;
+        try
+        {
+            //Первая часть
+            Console.WriteLine("Первая часть задания: ");
+            Console.WriteLine("Введите количество повторений");
+            n = double.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                sum += 1.0 / Math.Pow((i + 1), 3);
+            }
+            Console.WriteLine("Сумма после n-повторений = " + sum);
+
+            //Вторая часть
+            sum = 0;
+            double number = 1;
+            Console.WriteLine("Вторая часть задания: ");
+            Console.WriteLine("Введите точность(eps): ");
+            eps = double.Parse(Console.ReadLine());
+            while  (1.0 / Math.Pow((number + 1), 3) > eps)
+            {
+                sum += 1.0 / Math.Pow((number + 1), 3);
+                number++;
+            }
+            Console.WriteLine("Ваш ответ = " + sum);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Что-то пошло не по плану, проверь код");
+        }
+       
+    }
+}
